81ce147cf06f26820f2cd772d0c9df04669c9b45
Diego Scarpati <diegoscarpati13@gmail.com>
2022-06-06 18:42:03 -0300

    modificaciones de ruta a db

--- semiya/api/controllers/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const db = new Sequelize("semiYa", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
-
-module.exports = db;
--- semiya/api/server.js
+++ semiya/api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const db = require("./controllers/db");
+const db = require("./db");
 
 // const cookieParser = require("cookie-parser");
 // const session = require("express-session");
