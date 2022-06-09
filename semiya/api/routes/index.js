20b6e42187452b87f7531afd4199780798ee49c6
Tomas Pinto Kramer <tomaspintokramer@Air-de-Tomas.fibertel.com.ar>
2022-06-09 16:23:14 -0300

    cradas rutas de categorias, cerradas rutas usuarios

--- semiya/api/routes/index.js
+++ semiya/api/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const routerProducts = require("./products");
 const routerUsers = require("./users.js");
+const routerCategories = require("./categories");
 
 router.use('/products', routerProducts)
 router.use('/users', routerUsers)
+router.use('/cateogries', routerCategories)
 
 
 module.exports = router;
--- semiya/api/routes/users.js
+++ semiya/api/routes/users.js
@@ -3,51 +3,67 @@ const { Users } = require("../models");
 const UsersRouter = express.Router();
 const passport = require("passport");
 
+
+UsersRouter.post("/register", (req, res, next) => {
+  Users.create(req.body)
+  .then((user) => res.status(201).send(user))
+  .catch(next);
+});
+
+UsersRouter.post("/login", passport.authenticate("local"), (req, res) => {
+  req.login(req.user, function (err) {
+    if (err) {
+      return next(err);
+    }
+    return res.send(req.user);
+  });
+});
+
+UsersRouter.post("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    return res.sendStatus(200);
+  });
+});
+
+UsersRouter.put("/update/:id",(req, res,next)=>{
+  const id=req.params.id
+  Users.update(req.body,{where:{id}, returning:true})
+  .then(([affectedRows, user]) => {
+    res.send(user[0]);
+  })
+  .catch(next);
+});
+
+UsersRouter.put("/promote/:id",(req, res,next)=>{
+  const id=req.params.id
+  Users.update( { type: 'admin' },{where:{id}, returning:true})
+  .then(([affectedRows, user]) => {
+    res.send(user[0]);
+  })
+  .catch(next);
+});
+
+UsersRouter.get("/me", (req, res) => {
+  if (!req.user) {
+    return res.sendStatus(401);
+  }
+  res.send(req.user);
+});
+
+UsersRouter.delete("/:id",(req, res, next)=>{
+  const id=req.params.id
+    Users.destroy({where:{id}})
+    .then(()=>res.sendStatus(202))
+    .catch(next)
+})
+
 UsersRouter.get("/", (req, res, next)=>{
     Users.findAll()
     .then((users)=>res.send(users))
     .catch(next)
 })
 
-UsersRouter.post("/register", (req, res, next) => {
-    Users.create(req.body)
-      .then((user) => res.status(201).send(user))
-      .catch(next);
-  });
-  
-  UsersRouter.post("/login", passport.authenticate("local"), (req, res) => {
-    req.login(req.user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.send(req.user);
-    });
-  });
-  
-  UsersRouter.post("/logout", (req, res, next) => {
-    req.logout(function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.sendStatus(200);
-    });
-  });
-
-  UsersRouter.put("/:id/update",(req, res,next)=>{
-      const id=req.params.id
-      Users.update(req.body,{where:{id}, returning:true})
-      .then(([affectedRows, product]) => {
-        res.send(product[0]);
-      })
-      .catch(next);
-  });
-
-  UsersRouter.get("/me", (req, res) => {
-    if (!req.user) {
-      return res.sendStatus(401);
-    }
-  
-    res.send(req.user);
-  });
-  
-  module.exports= UsersRouter;
+module.exports= UsersRouter;
